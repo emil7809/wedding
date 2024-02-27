640ccbdf445de58c306843b5e86b93844287941a
emily <emil7809@gmail.com>
2024-02-27 13:47:30 +0100

    begin working on nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 
 import "./app.scss";
+import { useState } from "react";
 import Countdown from "./components/Countdown"
 import Menu from "./components/Menu"
+import Welcome from "./components/Welcome"
+
+
 
 function App() {
+
+  const [navigation, setNavigation] = useState("Menu")
+  const [active, setActive] = useState("")
 
 
   return (
     <>
       <Countdown />
       <main>
-        <Menu />
+        <div className={`${navigation === "Menu" ? "show" : "hide"}`} onClick={() => setNavigation("Welcome")}>
+          <Menu />
+        </div>
+        <div className={`${navigation === "Welcome" ? "show" : "hide"}`}>
+          <Welcome />
+        </div>
       </main>
-
+      {/*  <nav>
+        <div id="menu_one" className={`${active === "cards" ? "active_card_nav" : ""}`} onClick={() => { setNavigation("Cards Library"); setActive("cards") }} ></div>
+      </nav> */}
     </>
   )
 }
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <>
+            <section id="welcome">
+                <h1>Velkommen</h1>
+            </section>
+        </>
+    )
+}
