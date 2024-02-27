b72c8434daf059707991dcb4cc00371590fc6332
emily <emil7809@gmail.com>
2024-02-27 20:15:48 +0100

    onskeliste

--- src/components/Wishlist.tsx
+++ src/components/Wishlist.tsx
@@ -5,21 +5,30 @@ export default function Wishlist() {
         <>
             <section id="Wishlist">
                 <h2>Ønskeliste</h2>
-                <p>Det eneste vi ønsker er at kunne tage på honeymoon.
-                    Basically, vi vil hellere have dine penge end en personlig gave.
-                    Så får du en selfie med det, din gave blev brugt på.
-                </p>
+                <p>Det vi ønsker mest er at tage på honeymoon.</p>
+                <img src="https://i.imgflip.com/8h8zi7.jpg" alt="" />
+
+                <h3>Penge til Honeymoon</h3>
                 <div id='themo'>
                     <Thermometer
                         theme="dark"
-                        value="1"
-                        max="100.000"
-                        steps="3"
-                        format="Kr,-"
+                        value="0"
+                        max="20"
+                        steps="2"
+                        format="kr,-"
                         size="large"
                         height="300"
                     />
+
                 </div>
+                <h2>Andre Ønsker</h2>
+                <ul>
+                    <li>Lille kaffemaskine</li>
+                    <li>Sengetøj</li>
+                    <li>Pander</li>
+                    <li>Woks</li>
+                    <li>Tablet</li>
+                </ul>
             </section>
         </>
     )
