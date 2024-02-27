20eb3ac0deb95606e3527915d2688a5665c118aa
emily <emil7809@gmail.com>
2024-02-27 13:08:02 +0100

    menu up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
 import "./app.scss";
+import Countdown from "./components/Countdown"
+import Menu from "./components/Menu"
 
 function App() {
 
 
   return (
     <>
+      <Countdown />
+      <main>
+        <Menu />
+      </main>
 
     </>
   )
---
+++ src/components/Countdown.tsx
@@ -0,0 +1,26 @@
+export default function Countdown() {
+    return (
+        <>
+            <section id="countdown">
+                <div id="countdown_container">
+                    <div>
+                        <p id="dage"></p>
+                        <p>Dage</p>
+                    </div>
+                    <div>
+                        <p id="timer"></p>
+                        <p>Timer</p>
+                    </div>
+                    <div>
+                        <p id="min"></p>
+                        <p>Min</p>
+                    </div>
+                    <div>
+                        <p id="sec"></p>
+                        <p>Sec</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
